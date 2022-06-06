903fdd780c78c32281e73585516096c53b971c36
abdosalem490 <abdelrahman.ibrahim01@eng-st.cu.edu.eg>
2022-06-06 03:10:33 +0200

    initial commit

---
+++ database (missing)/code/Central Library/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿
+namespace Central_Library
+{
+    partial class loginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.userControl12 = new Central_Library.UserControl1();
+            this.userControl11 = new Central_Library.UserControl1();
+            this.userControl13 = new Central_Library.UserControl1();
+            this.SuspendLayout();
+            // 
+            // userControl12
+            // 
+            this.userControl12.BackColor = System.Drawing.SystemColors.Window;
+            this.userControl12.BorderColor = System.Drawing.Color.MediumSlateBlue;
+            this.userControl12.BorderFocusedState = System.Drawing.Color.HotPink;
+            this.userControl12.BorderRadius = 0;
+            this.userControl12.BorderSize = 2;
+            this.userControl12.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.userControl12.ForeColor = System.Drawing.Color.DimGray;
+            this.userControl12.Location = new System.Drawing.Point(927, 487);
+            this.userControl12.Margin = new System.Windows.Forms.Padding(4);
+            this.userControl12.Multiline = false;
+            this.userControl12.Name = "userControl12";
+            this.userControl12.Padding = new System.Windows.Forms.Padding(7);
+            this.userControl12.PasswordChar = false;
+            this.userControl12.Size = new System.Drawing.Size(250, 52);
+            this.userControl12.TabIndex = 1;
+            this.userControl12.Texts = "";
+            this.userControl12.UnderlinedStyle = false;
+            // 
+            // userControl11
+            // 
+            this.userControl11.BackColor = System.Drawing.SystemColors.Window;
+            this.userControl11.BorderColor = System.Drawing.Color.MediumSlateBlue;
+            this.userControl11.BorderFocusedState = System.Drawing.Color.HotPink;
+            this.userControl11.BorderRadius = 0;
+            this.userControl11.BorderSize = 2;
+            this.userControl11.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.userControl11.ForeColor = System.Drawing.Color.DimGray;
+            this.userControl11.Location = new System.Drawing.Point(917, 203);
+            this.userControl11.Margin = new System.Windows.Forms.Padding(4);
+            this.userControl11.Multiline = false;
+            this.userControl11.Name = "userControl11";
+            this.userControl11.Padding = new System.Windows.Forms.Padding(7);
+            this.userControl11.PasswordChar = false;
+            this.userControl11.Size = new System.Drawing.Size(250, 52);
+            this.userControl11.TabIndex = 0;
+            this.userControl11.Texts = "";
+            this.userControl11.UnderlinedStyle = false;
+            // 
+            // userControl13
+            // 
+            this.userControl13.BackColor = System.Drawing.SystemColors.Window;
+            this.userControl13.BorderColor = System.Drawing.Color.MediumSlateBlue;
+            this.userControl13.BorderFocusedState = System.Drawing.Color.HotPink;
+            this.userControl13.BorderRadius = 20;
+            this.userControl13.BorderSize = 2;
+            this.userControl13.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.userControl13.ForeColor = System.Drawing.Color.DimGray;
+            this.userControl13.Location = new System.Drawing.Point(312, 205);
+            this.userControl13.Margin = new System.Windows.Forms.Padding(4);
+            this.userControl13.Multiline = false;
+            this.userControl13.Name = "userControl13";
+            this.userControl13.Padding = new System.Windows.Forms.Padding(10, 7, 10, 7);
+            this.userControl13.PasswordChar = false;
+            this.userControl13.Size = new System.Drawing.Size(281, 52);
+            this.userControl13.TabIndex = 2;
+            this.userControl13.Texts = "";
+            this.userControl13.UnderlinedStyle = false;
+            // 
+            // loginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(2573, 963);
+            this.Controls.Add(this.userControl13);
+            this.Controls.Add(this.userControl12);
+            this.Controls.Add(this.userControl11);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "loginForm";
+            this.Padding = new System.Windows.Forms.Padding(21, 19, 21, 19);
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private UserControl1 userControl11;
+        private UserControl1 userControl12;
+        private UserControl1 userControl13;
+    }
+}
+
---
+++ database (missing)/code/Central Library/UserControl1.cs
@@ -0,0 +1,364 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Central_Library
+{
+
+    [DefaultEvent("_TextChanged")]
+    public partial class UserControl1 : UserControl
+    {
+        //Fields
+
+        private Color borderColor = Color.MediumSlateBlue;
+        private int borderSize = 2;
+        private bool underlinedStyle = false;
+        private Color borderFocusedState = Color.HotPink;
+        private bool isFocused = false;
+        private int borderRadius = 0;
+
+
+        //Color constructor
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+
+        //Events
+        public event EventHandler _TextChanged;
+
+
+        //properties
+        [Category("added Features")]
+        public Color BorderColor
+        {
+            get
+            {
+                return borderColor;
+            }
+
+            set
+            {
+                borderColor = value;
+                this.Invalidate();
+            }
+        }
+
+        [Category("added Features")]
+        public int BorderSize
+        {
+            get
+            {
+                return borderSize;
+            }
+
+            set
+            {
+                borderSize = value;
+                this.Invalidate();
+            }
+        }
+
+        [Category("added Features")]
+        public bool UnderlinedStyle
+        {
+            get
+            {
+                return underlinedStyle;
+            }
+            set
+            {
+                underlinedStyle = value;
+                this.Invalidate();
+            }
+        }
+
+        //overriden methods
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            Graphics graph = e.Graphics;
+
+            if (borderRadius > 1) //Rounded Textbox
+            {
+                //Fields
+                var rectBoarderSmooth = this.ClientRectangle;
+                var rectboarder = Rectangle.Inflate(rectBoarderSmooth, -borderSize, -borderSize);
+                int smoothSize = borderSize > 0 ? borderSize : 1;
+
+                using (GraphicsPath pathBorderSmooth = getFigurepath(rectBoarderSmooth, borderRadius))
+                using (GraphicsPath pathBorder = getFigurepath(rectboarder, borderRadius - borderSize))
+                using (Pen penBorderSmooth = new Pen(this.Parent.BackColor, smoothSize))
+                using (Pen penBorder = new Pen(BorderColor, borderSize))
+                {
+                    //drawing
+                    //draw border 
+
+                    this.Region = new Region(pathBorderSmooth); //set the rounded region of the user control
+                    if (borderRadius > 15) SetTextBoxRoundingRegion();  //set the rounded region of textbox component
+                    graph.SmoothingMode = SmoothingMode.AntiAlias;
+                    penBorder.Alignment = System.Drawing.Drawing2D.PenAlignment.Center;
+                    if (!isFocused)
+                    {
+                        if (underlinedStyle) //Line Style
+                        {
+                            //draw border smoothing
+                            graph.DrawPath(penBorderSmooth, pathBorderSmooth);
+                            //draw border
+                            graph.SmoothingMode = SmoothingMode.None;
+                            graph.DrawLine(penBorder, 0, this.Height - 1, this.Width - 1, this.Height - 1);
+                        }
+                        else // Normal Style
+                        {
+                            //draw border smoothing
+                            graph.DrawPath(penBorderSmooth, pathBorderSmooth);
+                            //draw border
+                            graph.DrawPath(penBorder, pathBorder);
+                        }
+                    }
+                    else
+                    {
+                        penBorder.Color = borderFocusedState;
+                        if (underlinedStyle) //Line Style
+                            graph.DrawLine(penBorder, 0, this.Height - 1, this.Width - 1, this.Height - 1);
+                        else // Normal Style
+                            graph.DrawRectangle(penBorder, 0, 0, this.Width - 0.5F, this.Height - 0.5F);
+                    }
+
+                }
+            }
+            else //Square/Normal Textbox
+            {
+                //draw border 
+                using (Pen penBorder = new Pen(BorderColor, borderSize))
+                {
+                    this.Region = new Region(this.ClientRectangle);
+                    penBorder.Alignment = System.Drawing.Drawing2D.PenAlignment.Inset;
+                    if (!isFocused)
+                    {
+                        if (underlinedStyle) //Line Style
+                            graph.DrawLine(penBorder, 0, this.Height - 1, this.Width - 1, this.Height - 1);
+                        else // Normal Style
+                            graph.DrawRectangle(penBorder, 0, 0, this.Width - 0.5F, this.Height - 0.5F);
+                    }
+                    else
+                    {
+                        penBorder.Color = borderFocusedState;
+                        if (underlinedStyle) //Line Style
+                            graph.DrawLine(penBorder, 0, this.Height - 1, this.Width - 1, this.Height - 1);
+                        else // Normal Style
+                            graph.DrawRectangle(penBorder, 0, 0, this.Width - 0.5F, this.Height - 0.5F);
+                    }
+                }
+            }
+
+
+        }
+
+        private void SetTextBoxRoundingRegion()
+        {
+            GraphicsPath pathTxt;
+            if(Multiline)
+            {
+                pathTxt = getFigurepath(textBox1.ClientRectangle, BorderRadius - borderSize);
+                textBox1.Region = new Region(pathTxt);
+            }
+            else
+            {
+                pathTxt = getFigurepath(textBox1.ClientRectangle,borderSize * 2);
+                textBox1.Region = new Region(pathTxt);
+            }
+        }
+
+        private GraphicsPath getFigurepath(Rectangle rect, int radius)
+        {
+            GraphicsPath path = new GraphicsPath();
+            float curveSize = radius * 2F;
+
+            path.StartFigure();
+            path.AddArc(rect.X, rect.Y, curveSize, curveSize, 100, 90);
+            path.AddArc(rect.Right - curveSize, rect.Y, curveSize, curveSize, 270, 90);
+            path.AddArc(rect.Right - curveSize, rect.Bottom - curveSize, curveSize, curveSize, 0, 90);
+            path.AddArc(rect.X, rect.Bottom - curveSize, curveSize, curveSize, 90, 90);
+            path.CloseFigure();
+            return path;
+
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+            if (this.DesignMode)
+                UpdateControlHeight();
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            UpdateControlHeight();
+        }
+
+        //private methos
+        private void UpdateControlHeight()
+        {
+
+            if (textBox1.Multiline == false)
+            {
+                int txtHeight = TextRenderer.MeasureText("Text", this.Font).Height + 1;
+                textBox1.Multiline = true;
+                textBox1.MinimumSize = new Size(0, txtHeight);
+                textBox1.Multiline = false;
+
+                this.Height = textBox1.Height + this.Padding.Top + this.Padding.Bottom;
+            }
+        }
+
+        [Category("added Features")]
+        public bool PasswordChar
+        {
+            get { return textBox1.UseSystemPasswordChar; }
+            set { textBox1.UseSystemPasswordChar = value; }
+        }
+
+        [Category("added Features")]
+        public bool Multiline
+        {
+            get { return textBox1.Multiline; }
+            set { textBox1.Multiline = value; }
+        }
+
+        [Category("added Features")]
+        public override Color BackColor
+        {
+            get
+            {
+                return base.BackColor;
+            }
+            set
+            {
+                base.BackColor = value;
+                textBox1.BackColor = value;
+            }
+        }
+
+        [Category("added Features")]
+        public override Color ForeColor
+        {
+            get
+            {
+                return base.ForeColor;
+            }
+            set
+            {
+                base.ForeColor = value;
+                textBox1.ForeColor = value;
+            }
+        }
+
+        [Category("added Features")]
+        public override Font Font
+        {
+            get
+            {
+                return base.Font;
+            }
+            set
+            {
+                base.Font = value;
+                textBox1.Font = value;
+                if (this.DesignMode)
+                    UpdateControlHeight();
+            }
+        }
+
+        [Category("added Features")]
+        public string Texts
+        {
+            get
+            {
+                return textBox1.Text;
+            }
+            set
+            {
+                textBox1.Text = value;
+            }
+        }
+
+        [Category("added Features")]
+        public Color BorderFocusedState
+        {
+            get
+            {
+                return borderFocusedState;
+            }
+            set
+            {
+                borderFocusedState = value;
+            }
+
+        }
+
+        [Category("added Features")]
+        public int BorderRadius
+        {
+            get
+            {
+                return borderRadius;
+            }
+            set
+            {
+                if (value >= 0)
+                {
+                    borderRadius = value;
+                    this.Invalidate();
+                }
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (_TextChanged != null)
+                _TextChanged.Invoke(sender, e);
+        }
+
+        private void textBox1_Click(object sender, EventArgs e)
+        {
+            this.OnClick(e);
+        }
+
+        private void textBox1_MouseEnter(object sender, EventArgs e)
+        {
+            this.OnMouseEnter(e);
+        }
+
+        private void textBox1_MouseLeave(object sender, EventArgs e)
+        {
+            this.OnMouseLeave(e);
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            this.OnKeyPress(e);
+        }
+
+        private void textBox1_Enter(object sender, EventArgs e)
+        {
+            isFocused = true;
+            this.Invalidate();
+        }
+
+        private void textBox1_Leave(object sender, EventArgs e)
+        {
+            isFocused = false;
+            this.Invalidate();
+        }
+    }
+
+}
